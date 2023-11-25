315aee069bb91deb44d677ab43f8a1ea3dd3784f
Rs-appez <alexandrep1999@hotmail.com>
2023-11-25 19:44:10 +0100

    add question view

--- chaloeil_backend/question/views.py
+++ chaloeil_backend/question/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import QuestionSerializer , CategorySerializer , AnswerSerializer
+
+from .models import Question , Answer , Category
+
+class QuestionViewSet(viewsets.ReadOnlyModelViewSet):
+    
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
