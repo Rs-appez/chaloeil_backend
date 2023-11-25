ef737b4146eeac4f65232ab9189c4084813efc10
Rs-appez <alexandrep1999@hotmail.com>
2023-11-25 19:42:53 +0100

    add question url

---
+++ chaloeil_backend/question/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'questions', views.QuestionViewSet,basename='question')
+router.register(r'categories', views.CategoryViewSet,basename='category')
+router.register(r'answers', views.AnswerViewSet,basename='answer')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
