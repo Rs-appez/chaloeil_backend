cf8e7a768e0cf4e80d2e01299d878c648f4d2415
Rs-appez <alexandrep1999@hotmail.com>
2023-11-25 19:43:28 +0100

    add model

--- chaloeil_backend/question/models.py
+++ chaloeil_backend/question/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    categories = models.ManyToManyField('Category')
+    level = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.question_text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.answer_text
+
+class Category(models.Model):
+    category_text = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.category_text
+
