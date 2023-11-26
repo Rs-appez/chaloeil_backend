06947020acf05adfaac6d68f17dbaca4cd5fbd92
Rs-appez <alexandrep1999@hotmail.com>
2023-11-26 17:28:25 +0100

    change questionViewset

--- chaloeil_backend/question/views.py
+++ chaloeil_backend/question/views.py
@@ -1,18 +1,18 @@
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated , IsAdminUser
 
 from .serializers import QuestionSerializer , CategorySerializer , AnswerSerializer
 
 from .models import Question , Answer , Category
 
-class QuestionViewSet(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [IsAuthenticated]
+class QuestionViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-    @action(detail=False, methods=['get'],url_path= 'random_question')
+    @action(detail=False, methods=['get'],url_path= 'random_question',permission_classes=[IsAuthenticated])
     def get_random_question(self, request, pk=None):
         question = Question.objects.order_by('?').first()
         serializer = QuestionSerializer(question, context={'request': request})
