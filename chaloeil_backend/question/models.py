c3e2c7bd696a9331641dc2546009e61732979421
Rs-appez <alexandrep1999@hotmail.com>
2023-11-26 17:35:16 +0100

    change question text type

--- chaloeil_backend/question/models.py
+++ chaloeil_backend/question/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.TextField()
     categories = models.ManyToManyField('Category')
     level = models.IntegerField()
     image_url = models.CharField(max_length=200, blank=True, null=True)
