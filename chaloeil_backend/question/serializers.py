1ca40f2edbdd8363e39f3ff8ea20db447c5afdc9
Rs-appez <alexandrep1999@hotmail.com>
2023-12-13 11:00:09 +0100

    Add 'id' field to QuestionSerializer

--- chaloeil_backend/question/serializers.py
+++ chaloeil_backend/question/serializers.py
@@ -22,7 +22,7 @@ class QuestionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Question
-        fields = ['question_text','categories','level','answers', 'image_url']
+        fields = ['id','question_text','categories','level','answers', 'image_url']
 
 class CategorySerializer(serializers.ModelSerializer):
 
