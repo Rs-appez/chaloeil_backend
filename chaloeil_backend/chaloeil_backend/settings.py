c6b91900b9894830fc3e8880b58f8328665fbb9d
Appez <37080870+Rs-appez@users.noreply.github.com>
2024-05-29 06:44:10 +0200

    add pagination

--- chaloeil_backend/chaloeil_backend/settings.py
+++ chaloeil_backend/chaloeil_backend/settings.py
@@ -146,5 +146,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
