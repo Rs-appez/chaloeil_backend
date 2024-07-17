91dbb824fb5587c15190d0a23e5984966ca76f5c
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-07-17 03:09:28 +0200

    remove unused import

--- chaloeil_backend/question/views.py
+++ chaloeil_backend/question/views.py
@@ -2,7 +2,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated , IsAdminUser
-from rest_framework.pagination import PageNumberPagination
 
 from .serializers import QuestionSerializer , CategorySerializer , AnswerSerializer
 
