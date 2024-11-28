8aa8e878d9903a4bf9b8b0695497a3e1159fa6eb
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-11-28 03:15:26 +0100

    setup word model

--- chaloeil_backend/mofus/admin.py
+++ chaloeil_backend/mofus/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Word
+
+class WordAdmin(admin.ModelAdmin):
+    list_editable = ['word']
+
+admin.site.register(Word)
--- chaloeil_backend/mofus/models.py
+++ chaloeil_backend/mofus/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from django.db import  models
+from django.forms import ValidationError
 
-# Create your models here.
+def case_unsensitive_validator(value):
+    if Word.objects.filter(word__iexact=value).exists():
+        raise ValidationError("The word already exists.")
+
+class Word(models.Model):
+    word = models.CharField(max_length=100, unique=True, validators=[case_unsensitive_validator])
+    length = models.IntegerField(editable=False)
+
+    def save(self, *args, **kwargs):
+        self.word = self.word.lower()
+        self.length = len(self.word)
+        super(Word, self).save(*args, **kwargs)
+            
+
+    def __str__(self):
+        return self.word
