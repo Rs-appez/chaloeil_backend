eb6132db5031760055a3ff61e7314dc45398b7aa
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-01-31 20:51:17 +0100

    add word view

--- chaloeil_backend/mofus/views.py
+++ chaloeil_backend/mofus/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Word
+from .serializers import WordSerializer
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+
+class WordViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Word.objects.all()
+    serializer_class = WordSerializer
