d7b16d80ca7c7f97e50f0806d698833cab29135b
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-06-24 16:58:52 +0200

    format

--- chaloeil_backend/question/serializers.py
+++ chaloeil_backend/question/serializers.py
@@ -1,34 +1,42 @@
-
-from .models import Question,Category,Answer,Level
+from .models import Question, Category, Answer, Level
 from rest_framework import serializers
 
-class LevelSerializer(serializers.ModelSerializer):
 
+class LevelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Level
-        fields = '__all__'
+        fields = "__all__"
+
 
 class AnswerSerializer(serializers.ModelSerializer):
-    id_question = serializers.PrimaryKeyRelatedField(queryset=Question.objects.all(), source='question')
+    id_question = serializers.PrimaryKeyRelatedField(
+        queryset=Question.objects.all(), source="question"
+    )
+
     class Meta:
         model = Answer
-        fields = ['id','answer_text','is_correct','id_question','emoticon']
-        
-class QuestionSerializer(serializers.ModelSerializer):
+        fields = ["id", "answer_text", "is_correct", "id_question", "emoticon"]
 
+
+class QuestionSerializer(serializers.ModelSerializer):
     categories = serializers.StringRelatedField(many=True)
     answers = AnswerSerializer(many=True)
     level = serializers.StringRelatedField(many=False)
 
     class Meta:
         model = Question
-        fields = ['id','question_text','categories','level','answers', 'image_url','shuffle_answers']
+        fields = [
+            "id",
+            "question_text",
+            "categories",
+            "level",
+            "answers",
+            "image_url",
+            "shuffle_answers",
+        ]
+
 
 class CategorySerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Category
-        fields = '__all__'
-
-
-
+        fields = "__all__"
