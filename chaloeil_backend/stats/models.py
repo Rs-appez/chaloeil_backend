636c595c40db79d52d5c8f6fad86be0136f483fa
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-06-24 22:18:44 +0200

    add stats model

--- chaloeil_backend/stats/models.py
+++ chaloeil_backend/stats/models.py
@@ -1,3 +1,113 @@
 from django.db import models
 
-# Create your models here.
+from question.models import Question, Answer
+
+
+class Participant(models.Model):
+    def is_player(self) -> bool:
+        """
+        Checks if the participant is a Player.
+        """
+        return hasattr(self, "player")
+
+    def is_team(self) -> bool:
+        """
+        Checks if the participant is a Team.
+        """
+        return hasattr(self, "team")
+
+    def get_real_instance(self):
+        """
+        Returns the real instance of the participant, which can be either a Player or a Team.
+        """
+        if hasattr(self, "player"):
+            return self.player
+        elif hasattr(self, "team"):
+            return self.team
+        return self  # Fallback to Participant
+
+    def __str__(self) -> str:
+        return f"{self.get_real_instance()}"
+
+
+class Player(Participant):
+    discord_id = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class TeamName(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Team(Participant):
+    players = models.ManyToManyField(Player, related_name="teams")
+    names = models.ManyToManyField(TeamName, related_name="teams")
+
+    def __str__(self) -> str:
+        return f"Team with {[', '.join(player.name for player in self.players.all())]}"
+
+    @staticmethod
+    def get_team_by_players(players):
+        """
+        Returns a team that contains all the players with the given IDs.
+        If no such team exists, returns None.
+        """
+        players = Player.objects.filter(discord_id__in=players)
+        player_ids = set(p.id for p in players)
+        for team in Team.objects.annotate(num_players=models.Count("players")).filter(
+            num_players=len(player_ids)
+        ):
+            team_player_ids = set(team.players.values_list("id", flat=True))
+            if team_player_ids == player_ids:
+                return team
+
+        return None
+
+    def add_name(self, name: str):
+        """
+        Adds a name to the team.
+        If the name already exists, it will not be added again.
+        """
+        team_name, created = TeamName.objects.get_or_create(name=name)
+        self.names.add(team_name)
+        return team_name
+
+class AnswerSelected(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    number_of_times = models.PositiveIntegerField(default=0)
+
+
+class Statistic(models.Model):
+    player = models.ForeignKey(Participant, on_delete=models.SET_NULL, null=True, blank=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    asked_count = models.PositiveIntegerField(default=0)
+    correct_count = models.PositiveIntegerField(default=0)
+    incorrect_count = models.PositiveIntegerField(default=0)
+
+    answers = models.ManyToManyField(AnswerSelected, related_name="statistics")
+
+    def __str__(self) -> str:
+        return f"Statistics for {self.player} on {self.question.question_text}"
+
+    def increment_asked_count(self, answer: Answer):
+        self.asked_count += 1
+
+        if answer:
+            if answer.is_correct:
+                self.correct_count += 1
+            else:
+                self.incorrect_count += 1
+
+            answer_selected, created = AnswerSelected.objects.get_or_create(
+                answer=answer
+            )
+            answer_selected.number_of_times += 1
+            answer_selected.save()
+        self.save()
---
+++ chaloeil_backend/stats/serializers.py
@@ -0,0 +1,64 @@
+from rest_framework import serializers
+
+from .models import Player, Team, Statistic, AnswerSelected, TeamName, Participant
+from question.serializers import QuestionSerializer, AnswerSerializer
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ["id", "discord_id", "name"]
+
+
+class TeamNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TeamName
+        fields = ["name"]
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    players = PlayerSerializer(many=True)
+    names = TeamNameSerializer(many=True)
+
+    class Meta:
+        model = Team
+        fields = ["id", "players", "names"]
+
+
+class AnswerSelectedSerializer(serializers.ModelSerializer):
+    answer = AnswerSerializer()
+
+    class Meta:
+        model = AnswerSelected
+        fields = ["answer", "number_of_times"]
+
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        if instance.is_player():
+            return PlayerSerializer(instance.get_real_instance()).data
+        elif instance.is_team():
+            return TeamSerializer(instance.get_real_instance()).data
+        return super().to_representation(instance)
+
+    class Meta:
+        model = Participant
+        fields = ["id"]
+
+
+class StatisticsSerializer(serializers.ModelSerializer):
+    player = ParticipantSerializer()
+    question = QuestionSerializer()
+    answers = AnswerSelectedSerializer(many=True)
+
+    class Meta:
+        model = Statistic
+        fields = [
+            "id",
+            "player",
+            "question",
+            "asked_count",
+            "correct_count",
+            "incorrect_count",
+            "answers",
+        ]
