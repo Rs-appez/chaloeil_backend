b35f151a68ad726df7380a3c60270ed3264e2fd4
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-07-06 01:55:20 +0200

    add Qotd model

--- chaloeil_backend/question/admin.py
+++ chaloeil_backend/question/admin.py
@@ -1,23 +1,27 @@
 from django.contrib import admin
 
-from .models import Answer, Category, Question , Level
+from .models import Answer, Category, Question, Level, QuestionsOfTheDay
+
 
 class AnswerInline(admin.TabularInline):
-  model = Answer
-  extra = 4
+    model = Answer
+    extra = 4
+
 
 class QuestionAdmin(admin.ModelAdmin):
-  inlines = [AnswerInline]  
-  save_as = True
-  list_display = ('question_text', 'display_categories', 'level')
-  list_filter = ['categories','level']
-  search_fields = ['question_text']
+    inlines = [AnswerInline]
+    save_as = True
+    list_display = ('question_text', 'display_categories', 'level')
+    list_filter = ['categories', 'level']
+    search_fields = ['question_text']
 
-  def display_categories(self, obj):
-    return ", ".join([category.category_text for category in obj.categories.all()])
-  display_categories.short_description = 'Categories'
+    def display_categories(self, obj):
+        return ", ".join([category.category_text for category in obj.categories.all()])
+    display_categories.short_description = 'Categories'
 
-admin.site.register(Question,QuestionAdmin)
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
 admin.site.register(Category)
 admin.site.register(Level)
+admin.site.register(QuestionsOfTheDay)
