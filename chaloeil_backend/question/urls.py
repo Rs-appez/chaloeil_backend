e786e0e721bc66d5b1d2664c17e357ec276d9e0a
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-07-06 02:03:15 +0200

    add qotd url

--- chaloeil_backend/question/urls.py
+++ chaloeil_backend/question/urls.py
@@ -3,11 +3,11 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'questions', views.QuestionViewSet,basename='question')
-router.register(r'categories', views.CategoryViewSet,basename='category')
-router.register(r'answers', views.AnswerViewSet,basename='answer')
+router.register(r'questions', views.QuestionViewSet, basename='question')
+router.register(r'categories', views.CategoryViewSet, basename='category')
+router.register(r'answers', views.AnswerViewSet, basename='answer')
+router.register(r'qotd', views.QuestionsOfTheDayViewSet, basename='qotd')
 
 urlpatterns = [
     path('', include(router.urls)),
 ]
-
