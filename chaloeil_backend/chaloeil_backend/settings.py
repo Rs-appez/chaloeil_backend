c5429e99ccaa35e189cd9d066df82ccecde95163
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-07-09 13:31:37 +0200

    remove cron

--- chaloeil_backend/chaloeil_backend/settings.py
+++ chaloeil_backend/chaloeil_backend/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 from decouple import config
 import dj_database_url
 
-from .cronjobs import CRONJOBS  # Import cron jobs
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
