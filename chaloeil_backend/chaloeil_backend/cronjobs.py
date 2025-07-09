29e3e7b1d270a2d9d583fa8981a67487bde834ec
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-07-09 13:31:37 +0200

    remove cron

--- chaloeil_backend/chaloeil_backend/cronjobs.py
+++
@@ -1,5 +0,0 @@
-CRONJOBS = [
-    ('0 18 * * *', 'question.cron.create_questions_of_the_day',
-     '>> /tmp/django_cron_test.log 2>&1'),
-
-]
--- chaloeil_backend/question/cron.py
+++
@@ -1,9 +0,0 @@
-from .models import QuestionsOfTheDay
-import random
-
-def create_questions_of_the_day():
-    """
-    Create or update the Questions of the Day.
-    """
-    number_of_questions = random.randint(15, 25)
-    QuestionsOfTheDay(number_of_questions=number_of_questions).save()
