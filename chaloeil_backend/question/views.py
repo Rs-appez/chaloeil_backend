d472c3d0c3dd6fe23d5220fb83a87217c6680f93
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-19 02:51:32 +0200

    feat(permission): use DjangoModelPermissions for QOTD endpoints
    Replace IsAuthenticated and IsAdminUser with DjangoModelPermissions for
    the get_qotd and generate_qotd actions in QuestionsOfTheDayViewSet.
    This change enforces model-level permissions for these endpoints,
    aligning access control with Django's permission system.

--- chaloeil_backend/question/views.py
+++ chaloeil_backend/question/views.py
@@ -4,7 +4,11 @@ import random
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import (
+    IsAdminUser,
+    IsAuthenticated,
+    DjangoModelPermissions,
+)
 from rest_framework.response import Response
 from stats.models import PlayerQotd
 
@@ -135,7 +139,7 @@ class QuestionsOfTheDayViewSet(viewsets.ModelViewSet):
         detail=False,
         methods=["get"],
         url_path="qotd",
-        permission_classes=[IsAuthenticated],
+        permission_classes=[DjangoModelPermissions],
     )
     def get_qotd(self, request, pk=None):
         """
@@ -172,7 +176,7 @@ class QuestionsOfTheDayViewSet(viewsets.ModelViewSet):
         detail=False,
         methods=["post"],
         url_path="generate_qotd",
-        permission_classes=[IsAdminUser],
+        permission_classes=[DjangoModelPermissions],
     )
     def generate_qotd(self, request, pk=None):
         """
