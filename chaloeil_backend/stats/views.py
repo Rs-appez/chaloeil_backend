c66b6ac3bd0893ba8a856565d62c1897d2db86dd
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-21 14:18:41 +0200

    add perm on view

--- chaloeil_backend/stats/views.py
+++ chaloeil_backend/stats/views.py
@@ -1,15 +1,18 @@
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
-
 from question.models import QuestionsOfTheDay
+
 from .models import Statistic
 
 
+@permission_required("chaloeil_backend.view_statistic")
 def index(request):
     stats = Statistic.objects.all()
     data = Statistic.get_pourcentage(stats)
     return render(request, "index1.html", {"data": data})
 
 
+@permission_required("chaloeil_backend.view_statistic")
 def qotd_stats(request):
     qotd = QuestionsOfTheDay.objects.last()
     if not qotd:
